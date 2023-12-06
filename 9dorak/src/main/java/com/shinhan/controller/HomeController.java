e2fd84f3e4a8a7f43a59a2a90c303a2cc9f94ed7
Han-JB15 <80495508+Han-JB15@users.noreply.github.com>
2023-12-06 11:27:10 +0900

    Feat: 메인 화면 수정

--- 9dorak/src/main/java/com/shinhan/controller/HomeController.java
+++ 9dorak/src/main/java/com/shinhan/controller/HomeController.java
@@ -2,15 +2,22 @@ package com.shinhan.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shinhan.dto.ChallengeVO;
+import com.shinhan.dto.ProVO;
+import com.shinhan.model.ChallengeService;
+import com.shinhan.model.MenuService;
 
 /**
  * Handles requests for the application home page.
@@ -18,13 +25,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	MenuService menuService;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
@@ -33,10 +42,11 @@ public class HomeController {
 	}
 	
 	@GetMapping("/main.do")
-	public String main () {
+	public String allergy_menu(Model model) {
+		List<ProVO> prolist = menuService.selectAll();
+		System.out.println(prolist);
+		model.addAttribute("prolist", prolist);
 		return "main";
 	}
 	
-	
-
 }
