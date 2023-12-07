dd1e7017874a1e8181d67571a09d35e161d98197
Sunhee <sunhcho227@gmail.com>
2023-12-07 21:08:55 +0900

    Feat : wallet

--- 9dorak/src/main/java/com/shinhan/model/WalletService.java
+++ 9dorak/src/main/java/com/shinhan/model/WalletService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.shinhan.dto.BasketVO;
+import com.shinhan.dto.PayVO;
 
 @Service
 public class WalletService {
@@ -11,7 +12,11 @@ public class WalletService {
 	@Autowired
 	WalletDAOMybatis dao;
 
-	public List<BasketVO> selectAll() {
-		return dao.selectAll();
+	public List<BasketVO> selectAllBasket() {
+		return dao.selectAllBasket();
+	}
+	
+	public List<PayVO> selectAllPay() {
+		return dao.selectAllPay();
 	}
 }
