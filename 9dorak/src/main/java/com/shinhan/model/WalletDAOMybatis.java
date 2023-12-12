8be4db88bfd28ad828b5048ed3ab822b7043360f
Sunhee <sunhcho227@gmail.com>
2023-12-12 11:53:39 +0900

    Feat : basket

--- 9dorak/src/main/java/com/shinhan/model/WalletDAOMybatis.java
+++ 9dorak/src/main/java/com/shinhan/model/WalletDAOMybatis.java
@@ -1,6 +1,7 @@
 package com.shinhan.model;
 
 import java.util.List;
+import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,13 +18,13 @@ public class WalletDAOMybatis {
 	Logger logger = LoggerFactory.getLogger(WalletDAOMybatis.class);
 	String NAMESPACE = "net.firstzone.wallet.";
 	
-	// 장바구니 select all
-	public List<BasketVO> selectAllBasket() {
-		List<BasketVO> blist = sqlSession.selectList(NAMESPACE + "selectAllBasket");
-		System.out.println(blist);
-		logger.info("selectAllBasket :  {}", blist.size());
-		return blist;
-	}
+//	// 장바구니 select all
+//	public List<BasketVO> selectAllBasket() {
+//		List<BasketVO> blist = sqlSession.selectList(NAMESPACE + "selectAllBasket");
+//		System.out.println(blist);
+//		logger.info("selectAllBasket :  {}", blist.size());
+//		return blist;
+//	}
 	
 	// 결제하기 select all
 	public List<PayVO> selectAllPay() {
@@ -34,4 +35,37 @@ public class WalletDAOMybatis {
 	}
 	
 	
+	
+	
+	
+	
+	
+	
+    // 장바구니 추가
+    public int addBasket(BasketVO basket) {
+        int result = sqlSession.insert(NAMESPACE + "addBasket", basket);
+        return result;
+    }
+
+    // 장바구니 삭제
+    public int deleteBasket(String mem_id, int pro_no) {
+        int result = sqlSession.delete(NAMESPACE + "deleteBasket", Map.of("mem_id", mem_id, "pro_no", pro_no));
+        return result;
+    }
+
+    // 장바구니 수량 수정
+    public int modifyCount(BasketVO basket) {
+        int result = sqlSession.update(NAMESPACE + "modifyCount", basket);
+        return result;
+    }
+
+    // mem_id의 장바구니 목록
+    public List<BasketVO> getBasket(String mem_id) {
+        return sqlSession.selectList(NAMESPACE + "getBasket", mem_id);
+    }
+
+    // mem_id의 장바구니 존재 여부 확인
+    public int checkBasket(String mem_id) {
+        return sqlSession.selectOne(NAMESPACE + "checkBasket", mem_id);
+    }
 }
