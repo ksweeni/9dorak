9ce8cf2f74baf133debc4fa4388f4a6df9db6cae
purbluue <143508656+purbluue@users.noreply.github.com>
2023-12-12 16:58:48 +0900

    Feat: 카카오api 완성
    로그인,로그아웃

--- 9dorak/src/main/java/com/shinhan/controller/LoginController.java
+++ 9dorak/src/main/java/com/shinhan/controller/LoginController.java
@@ -137,11 +137,14 @@ public class LoginController {
 	        
 	    }else {
 
-	        MemVO loginCheck = lservice.userKakaoLoginPro(paramMap);
+	        MemVO kakaologin = lservice.userKakaoLoginPro(paramMap);
 	        
 	        //로그인 정보를 세션에 저장
-	        session.setAttribute("kakaologin", loginCheck);
+	        session.setAttribute("loginmem", kakaologin);
 	        resultMap.put("JavaData", "YES");
+	        
+	        //MemVO loginInfo = (MemVO) session.getAttribute("kakaologin");
+	        //System.out.println(loginInfo);
 	    }
 
 	    return resultMap;
