ac6a5426ca97672cf88dd14ae7041f1408349f30
Sunhee <sunhcho227@gmail.com>
2023-12-29 12:43:38 +0900

    Feat : orderDetail

--- 9dorak/src/main/java/com/shinhan/dto/OrderdetailVO.java
+++ 9dorak/src/main/java/com/shinhan/dto/OrderdetailVO.java
@@ -15,8 +15,4 @@ public class OrderdetailVO {
 	private int order_no; // 주문 고유 번호
 	private int pro_no; // 상품 고유 번호
 	private int orderdetail_count; // 상품 당 주문 갯수
-	
-	//
-	private String pro_name;
-
 }
